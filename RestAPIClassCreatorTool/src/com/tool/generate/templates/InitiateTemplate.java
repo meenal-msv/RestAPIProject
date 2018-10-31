4eb3248448709323b399b4bce7044c4e9da50bf9
Meenal Sahasrabudhe <meenal.sahasrabudhe@misys.com>
2018-10-31 13:30:03 +0530

    RestAPI workspace commit

---
+++ RestAPIClassCreatorTool/src/com/tool/generate/templates/InitiateTemplate.java
@@ -0,0 +1,5 @@
+package com.tool.generate.templates;
+
+public class InitiateTemplate {
+	
+}
---
+++ RestAPIClassCreatorTool/src/com/tool/generate/templates/SubmitTemplate.java
@@ -0,0 +1,12 @@
+package com.tool.generate.templates;
+
+import java.io.File;
+
+public class SubmitTemplate {
+
+	public void submitControllerCreation()
+	{
+		String folderPath = "V:/MeenalSV/Workspaces/RestAPI/RestAPIDummy/src/main/resources/webservices/apis/controller";
+		File file = new File(folderPath+"/Submit.java");
+	}
+}
